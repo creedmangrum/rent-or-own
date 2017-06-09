7d760ef18df4866b4f54562263bfced29060fa02
creedmangrum <creed.mangrum@gmail.com>
2017-06-09 15:17:49 -0400

    updates

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,80 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <h1>Calculate whether you'll lose more money renting an apartment or buying a home</h1>
     <div class="form">
-      <div>
-        <span class="form-item-label">Home Cost</span>
-        <input class="form-item" type="number" v-model="homeCost" placeholder="home cost" >
+      <div class="form-home-costs">
+        <div>
+          <span class="form-item-label">Home Cost</span>
+          <input class="form-item" type="number" v-model="homeCost" placeholder="home cost" >
+        </div>
+        <div>
+          <span class="form-item-label">Down Payment</span>
+          <input class="form-item" type="number" v-model="downPayment" placeholder="down payment" >
+        </div>
+        <div>
+          <span class="form-item-label">Closing Costs</span>
+          <input class="form-item" type="number" v-model="closingCosts" placeholder="closing costs" >
+        </div>
+        <div>
+          <span class="form-item-label">Mortgage Length (in years)</span>
+          <input class="form-item" type="number" v-model="mortgageLength" placeholder="mortgage length (in years)" >
+        </div>
+        <div>
+          <span class="form-item-label">Loan Interest Rate</span>
+          <input class="form-item" type="number" v-model="interestRate" placeholder="loan interest rate" >
+        </div>
+        <div>
+          <span class="form-item-label">Annual Property Taxes</span>
+          <input class="form-item" type="number" v-model="yearlyPropertyTaxes" placeholder="yearly property taxes" >
+        </div>
+        <div>
+          <span class="form-item-label">Annual Insurance</span>
+          <input class="form-item" type="number" v-model="annualInsurance" placeholder="annual insurance" >
+        </div>
+        <div>
+          <span class="form-item-label">Mortgage Insurance Rate</span>
+          <input class="form-item" type="number" v-model="mortgageInsuranceRate" placeholder="mortgageInsuranceRate" >
+        </div>
       </div>
-      <div>
-        <span class="form-item-label">Down Payment</span>
-        <input class="form-item" type="number" v-model="downPayment" placeholder="down payment" >
+      <div class="form-rent-costs">
+        <div>
+          <span class="form-item-label">Monthly Rent</span>
+          <input class="form-item" type="number" v-model="rentMonthCost" placeholder="rent per month" >
+        </div>
+        <div>
+          <span class="form-item-label">Renters Insurance</span>
+          <input class="form-item" type="number" v-model="rentInsurance" placeholder="renters insurance (annual)" >
+        </div>
       </div>
-      <div>
-        <span class="form-item-label">Closing Costs</span>
-        <input class="form-item" type="number" v-model="closingCosts" placeholder="closing costs" >
+    </div>
+    <div class="totals">
+      <div class="totals-home">
+        <div>
+          <span class="output-item">Total Due at Closing</span>
+          <span class="output-item">${{ dueAtClose }}</span>
+        </div>
+        <div>
+          <span class="output-item">Monthly Mortgage Payment</span>
+          <span class="output-item">${{ monthlyMortgagePayment }}</span>
+        </div>
+        <div>
+          <span class="output-item">Monthly Mortgage Insurance</span>
+          <span class="output-item">${{ monthlyMortgageInsurance }}</span>
+        </div>
+        <div>
+          <span class="output-item">Total Monthly Payment</span>
+          <span class="output-item">${{ monthlyTotalPayment }}</span>
+        </div>
       </div>
-      <div>
-        <span class="form-item-label">Mortgage Length</span>
-        <input class="form-item" type="number" v-model="mortgageLength" placeholder="mortgage length (in years)" >
-      </div>
-      <div>
-        <span class="form-item-label">Loan Interest Rate</span>
-        <input class="form-item" type="number" v-model="interestRate" placeholder="loan interest rate" >
-      </div>
-      <div>
-        <span class="form-item-label">Annual Property Taxes</span>
-        <input class="form-item" type="number" v-model="yearlyPropertyTaxes" placeholder="yearly property taxes" >
-      </div>
-      <div>
-        <span class="form-item-label">Annual Insurance</span>
-        <input class="form-item" type="number" v-model="annualInsurance" placeholder="annual insurance" >
-      </div>
-      <div>
-        <span class="form-item-label">Mortgage Insurance Rate</span>
-        <input class="form-item" type="number" v-model="mortgageInsuranceRate" placeholder="mortgageInsuranceRate" >
-      </div>
-      <div>
-        <span class="output-item">Monthly Mortgage Payment</span>
-        <span class="output-item">{{ monthlyMortgagePayment }}</span>
-      </div>
-      <div>
-        <span class="output-item">Monthly Mortgage Insurance</span>
-        <span class="output-item">{{ monthlyMortgageInsurance }}</span>
-      </div>
-      <div>
-        <span class="output-item">Total Monthly Payment</span>
-        <span class="output-item">{{ monthlyTotalPayment }}</span>
+      <div class="totals-rent">
+        <div>
+          <span class="output-item">Annual Rent Cost</span>
+          <span class="output-item">${{ rentAnnual }}</span>
+        </div>
+        <div>
+          <span class="output-item">Total to Rent</span>
+          <span class="output-item">${{ totalRentAnnual }}</span>
+        </div>
       </div>
     </div>
   </div>
@@ -64,22 +93,37 @@ export default {
       mortgageInsuranceRate: .003,
       mortgageLength: 30,
       msg: 'Welcome to Your Vue.js App that is cool',
+      rentInsurance:  500,
+      rentMonthCost: 1900,
       yearlyPropertyTaxes: 5200
     }
   },
   computed: {
+    dueAtClose: function() {
+      return this.downPayment + this.closingCosts;
+    },
     loanAmount: function() {
       return this.homeCost - this.downPayment;
     },
     monthlyMortgageInsurance: function() {
-      return this.loanAmount * this.mortgageInsuranceRate / 12;
+      return parseFloat(this.loanAmount * this.mortgageInsuranceRate / 12).toFixed(2);
     },
     monthlyMortgagePayment: function() {
-      return ( (this.homeCost - this.downPayment) * (this.interestRate / 12) ) / (1 - Math.pow((1 + (this.interestRate/12)), (-1 * this.mortgageLength * 12)));
+      return parseFloat(( (this.homeCost - this.downPayment) * (this.interestRate / 12) ) / (1 - Math.pow((1 + (this.interestRate/12)), (-1 * this.mortgageLength * 12)))).toFixed(2);
     },
     monthlyTotalPayment: function() {
-      return this.monthlyMortgagePayment + this.annualInsurance / 12 + this.yearlyPropertyTaxes / 12 + this.monthlyMortgageInsurance;
-    }
+      return parseFloat(parseFloat(this.monthlyMortgagePayment) + this.annualInsurance / 12 + this.yearlyPropertyTaxes / 12 + this.monthlyMortgageInsurance).toFixed(2);
+    },
+    rentAnnual: function() {
+      return parseFloat(this.rentMonthCost * 12).toFixed(2);
+    },
+    totalRentAnnual: function() {
+      return parseFloat( parseFloat(this.rentAnnual) + this.rentInsurance ).toFixed(2);
+    },
+  },
+  toDollar: function(numberSting) {
+    let [dollars, cents] = numberString.split('.');
+
   }
 }
 </script>
@@ -96,6 +140,31 @@ export default {
 
 h1, h2 {
   font-weight: normal;
+}
+
+.form {
+  display: flex;
+}
+
+.form-home-costs {
+  flex: 1 1 50%;
+}
+
+.form-rent-costs {
+  flex: 1 1 50%;
+}
+
+.totals {
+  display: flex;
+  margin-top: 5rem;
+}
+
+.totals-home {
+  flex: 1 1 50%;
+}
+
+.totals-rent {
+  flex: 1 1 50%;
 }
 
 input {
